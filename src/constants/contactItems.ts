affc7767c3c78b739678331be0f1ab1e4ff15bd4
ervand96 <ervandharutyunyan06096@gmail.com>
2026-05-08 23:29:45 +0400

    fixed

--- src/constants/contactItems.ts
+++ src/constants/contactItems.ts
@@ -32,13 +32,13 @@ export const contactData: ContactItem[] = [
     key: "Viber",
     value: "+33-609-57-27-80",
     sub: "ConvenientCommunication",
-    href: "https://viber.com",
+    href: "https://viber.me/+33609572780",
   },
   {
     icon: whatsAppIcon,
     key: "WhatsApp",
     value: "+33-609-57-27-80",
     sub: "QuickResponses",
-    href: "https://wa.me",
+    href: "https://wa.me/33609572780",
   },
 ];
