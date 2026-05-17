5709c32b61cf2152c9cecd80f8f1c7113d42ac86
ervand96 <ervandharutyunyan06096@gmail.com>
2026-05-17 18:26:14 +0400

    cfg: remove static export to allow API routes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 const nextConfig = {
-  output: "export",
   trailingSlash: false,
   images: {
     remotePatterns: [
