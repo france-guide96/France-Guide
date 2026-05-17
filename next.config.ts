b676bc6582e6f718580523809258bae80e8c4ec6
ervand96 <ervandharutyunyan06096@gmail.com>
2026-05-17 18:24:32 +0400

    cfg: enable static html export output for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 const nextConfig = {
+  output: "export",
   trailingSlash: false,
   images: {
     remotePatterns: [
