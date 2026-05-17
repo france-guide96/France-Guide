f40f24fb2a4c55b1c21fff44b22e0fe1404a85f5
ervand96 <ervandharutyunyan06096@gmail.com>
2026-05-17 18:17:53 +0400

    fix: remove edge runtime to support static params build

--- src/app/[locale]/(main)/transfer/page.tsx
+++ src/app/[locale]/(main)/transfer/page.tsx
@@ -1,5 +1,3 @@
-export const runtime = 'edge';
-
 import ReviewSectionServer from "@/app/features/ReviewSectionServer";
 import TransferPage from "@/app/features/TransferComponent";
 import Container from "@/app/shared/Container";
