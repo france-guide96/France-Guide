12d178ef57a045f4f64d4391b078f76c46a99a73
france-guide96 <guidefrance7@gmail.com>
2026-06-05 17:14:04 +0400

    deploy

--- src/app/shared/HeroSection/index.tsx
+++ src/app/shared/HeroSection/index.tsx
@@ -46,7 +46,7 @@ export default function HeroSection({
   };
 
   return (
-    <section className="relative min-h-screen overflow-hidden">
+    <section className="relative min-h-screen overflow-hidden"> 
       <video
         autoPlay
         muted
